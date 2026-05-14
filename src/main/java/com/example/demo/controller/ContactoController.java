85b2d94e5f6b22fc095c2342308af4cb5891c0ef
Alain <butronvelezkevinalain@gmail.com>
2026-05-14 01:55:20 -0600

    feat: add ContactoController and migrate project to MariaDB with updated Spring Boot dependencies

--- src/main/java/com/example/demo/controller/ContactoController.java
+++ src/main/java/com/example/demo/controller/ContactoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/contactos")
 @CrossOrigin(origins = "http://localhost:4200")
+@SuppressWarnings("null")
 public class ContactoController {
 
     @Autowired
